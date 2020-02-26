2a2d29b51d98a4fff94db3ad4b30bf9466497e1b
Mark DeNyse <mark@driftwoodinteractive.com>
2020-02-26 09:21:55 -0500

    Update fmRecord.class.php
    Changed fmRecord's getFieldData() to get first repetition even when $repetition is passed as 0.

--- fmPDA/v2/_fmPDA/fmRecord.class.php
+++ fmPDA/v2/_fmPDA/fmRecord.class.php
@@ -102,18 +102,27 @@ class fmRecord
       return $result;
    }
 
+   function delete()
+   {
+      return $this->fm->apiDeleteRecord($this->layout, $this->getRecordId());
+   }
+
    private function getFieldData($field, $repetition = 0, $decode = true)
    {
       $result = '';
 
-      if (array_key_exists($field, $this->data[FM_FIELD_DATA])) {
-         if ($repetition == 0) {
-            $result =  $this->data[FM_FIELD_DATA][$field];
-         }
-         else {
-            $result =  $this->data[FM_FIELD_DATA][$field .'('. $repetition .')'];
-         }
+      if (array_key_exists($field .'('. $repetition .')', $this->data[FM_FIELD_DATA])) {              // Try exact repetition
+         $result =  $this->data[FM_FIELD_DATA][$field .'('. $repetition .')'];
       }
+
+      else if (($repetition == 0) && array_key_exists($field .'(1)', $this->data[FM_FIELD_DATA])) {   // See if it's really rep 1
+         $result =  $this->data[FM_FIELD_DATA][$field .'(1)'];
+      }
+
+      else if (array_key_exists($field, $this->data[FM_FIELD_DATA])) {                                // No repetition
+         $result =  $this->data[FM_FIELD_DATA][$field];
+      }
+
       else {
          fmLogger(__METHOD__ .'(): '. $field .' does not exist. Is it missing from your FileMaker layout?');
       }
